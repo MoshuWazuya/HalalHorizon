8f3ca59f89f452ead6cc544bfdd47ce1973576a9
friessama <fs561317@gmail.com>
2025-01-06 12:48:43 +0800

    update lagi for checkoutpage

---
+++ checkoutpage.js
@@ -0,0 +1,4 @@
+document.getElementById("checkout-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Your travel details have been submitted successfully!");
+});
